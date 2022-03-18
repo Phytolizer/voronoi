d838d77430354cea09b03ef57e91544fafe04f5c
Kyle Coffey <me@phytolizer.dev>
2022-03-18 14:12:45 -0500

    basic window

--- source/main.cpp
+++ source/main.cpp
@@ -1,2 +1,11 @@
-int main() {
+#include <SDL.h>
+
+int main(int argc, char** argv) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window =
+        SDL_CreateWindow("voronoi diagram", SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED, 800, 600, 0);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
