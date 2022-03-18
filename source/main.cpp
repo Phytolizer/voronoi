e3327b62e55f9183677be8931335fde3f2f52ea2
Kyle Coffey <me@phytolizer.dev>
2022-03-18 14:16:05 -0500

    suppress warning

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,8 @@
 #include <SDL.h>
 
 int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window =
         SDL_CreateWindow("voronoi diagram", SDL_WINDOWPOS_UNDEFINED,
